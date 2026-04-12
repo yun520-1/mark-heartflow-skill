640d7c3fa3a21f275e3376b8e1a688b8c5a8cc64
yun520-1 <yun520-1@users.noreply.github.com>
2026-04-12 20:34:10 +0800

    HeartFlow v7.6.011 - Auto Upgrade

--- src/core/super-integration.js
+++ src/core/super-integration.js
@@ -158,7 +158,7 @@ function loadAllModules(projectRoot) {
 class SuperHeartFlowEngine {
   constructor(projectRoot) {
     this.projectRoot = projectRoot;
-    this.version = '7.6.010';
+    this.version = '7.6.011';
     this.startTime = Date.now();
 
     // 初始化所有子系统
