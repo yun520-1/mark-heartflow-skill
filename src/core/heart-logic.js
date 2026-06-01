9bd5b233185c837431ad72f322f724e6dd359f1e
yun520-1 <yun520-1@users.noreply.github.com>
2026-06-01 10:02:12 +0800

    auto: 2026-06-01_10 upgrade

--- src/core/heart-logic.js
+++ src/core/heart-logic.js
@@ -5,7 +5,7 @@
 class HeartLogic {
   constructor() {
     this.name = 'HeartLogic';
-    this.version = '2.0.10';
+    this.version = '2.0.11';
     this.isRunning = true;
     this.thoughtHistory = [];
     this.lastInteraction = Date.now();
